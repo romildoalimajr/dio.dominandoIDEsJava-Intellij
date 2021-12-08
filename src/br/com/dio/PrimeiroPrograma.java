53ba42994c4cbe393fb5e54124c0c795b08907ac
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2021-12-08 20:32:31 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+
+       /* int a = 10;
+        int b = 3;
+        System.out.println("Hello World!!!" + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
